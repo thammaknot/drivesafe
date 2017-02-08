74477422ad17fae83c96b79c2f93411890d316da
Knot <thammaknot@gmail.com>
2017-02-08 23:08:50 +0700

    added checkpoint manager

--- app/src/main/java/com/knottycode/drivesafe/CheckpointManager.java
+++ app/src/main/java/com/knottycode/drivesafe/CheckpointManager.java
@@ -44,7 +44,7 @@ public class CheckpointManager {
             calculateNextFrequency();
         }
     }
-    
+        
     public long getNextFrequencyMillis() {
         return nextFrequencyMillis;
     }
