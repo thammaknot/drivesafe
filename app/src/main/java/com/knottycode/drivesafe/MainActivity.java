7e64ea374f68444a5d4e60201a9aebbdade8823b
Knot <thammaknot@gmail.com>
2017-02-27 00:22:28 +0700

    basic framework setup

--- app/src/main/java/com/knottycode/drivesafe/MainActivity.java
+++ app/src/main/java/com/knottycode/drivesafe/MainActivity.java
@@ -1,11 +1,14 @@
 package com.knottycode.drivesafe;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
@@ -16,6 +19,10 @@ import java.util.HashSet;
 import java.util.List;
 
 import static com.google.android.gms.internal.zzs.TAG;
+import static com.knottycode.drivesafe.R.id.alarmTones;
+import static com.knottycode.drivesafe.R.id.checkpointFrequency;
+import static com.knottycode.drivesafe.R.id.checkpointFrequencyDisplay;
+import static com.knottycode.drivesafe.R.id.recordTone;
 
 public class MainActivity extends Activity {
 
@@ -36,7 +43,7 @@ public class MainActivity extends Activity {
 
         setContentView(R.layout.activity_main);
 
-        checkpointFrequencyTextView = (TextView) findViewById(R.id.checkpointFrequencyDisplay);
+        checkpointFrequencyTextView = (TextView) findViewById(checkpointFrequencyDisplay);
     }
 
     @Override
@@ -68,28 +75,30 @@ public class MainActivity extends Activity {
         int seconds = checkpointFreqSeconds % 60;
         checkpointFrequencyTextView.setText(String.format("%02d:%02d", minutes, seconds));
         if (adaptiveCheckpointFrequency) {
-            findViewById(R.id.icon1).setBackground(getResources().getDrawable(R.drawable.icon1));
+            findViewById(R.id.adaptiveCheckpointFrequencIcon)
+                    .setBackground(getResources().getDrawable(R.drawable.adaptive_checkpoint_icon));
+            findViewById(R.id.adaptiveCheckpointFrequencIcon).setVisibility(View.VISIBLE);
         } else {
-            findViewById(R.id.icon1).setBackground(getResources().getDrawable(R.drawable.icon1_disabled));
+            findViewById(R.id.adaptiveCheckpointFrequencIcon).setVisibility(View.INVISIBLE);
         }
         switch (alertMode) {
             case SCREEN:
-                findViewById(R.id.icon2).setVisibility(View.VISIBLE);
-                findViewById(R.id.icon2).setBackground(getResources().getDrawable(R.drawable.icon2));
+                findViewById(R.id.alertTypeIcon).setVisibility(View.INVISIBLE);
                 break;
             case VIBRATE:
-                findViewById(R.id.icon2).setVisibility(View.VISIBLE);
-                findViewById(R.id.icon2).setBackground(getResources().getDrawable(R.drawable.icon2_disabled));
+                findViewById(R.id.alertTypeIcon).setVisibility(View.INVISIBLE);
                 break;
             case SOUND:
-                findViewById(R.id.icon2).setVisibility(View.INVISIBLE);
+                findViewById(R.id.alertTypeIcon).setVisibility(View.VISIBLE);
                 break;
             default:
         }
         if (adaptiveLoudness) {
-            findViewById(R.id.icon3).setBackground(getResources().getDrawable(R.drawable.icon3));
+            findViewById(R.id.adaptiveLoudnessIcon)
+                    .setBackground(getResources().getDrawable(R.drawable.adaptive_loudness_icon));
+            findViewById(R.id.adaptiveLoudnessIcon).setVisibility(View.VISIBLE);
         } else {
-            findViewById(R.id.icon3).setBackground(getResources().getDrawable(R.drawable.icon3_disabled));
+            findViewById(R.id.adaptiveLoudnessIcon).setVisibility(View.INVISIBLE);
         }
     }
 
@@ -102,6 +111,55 @@ public class MainActivity extends Activity {
     public void enterSettings(View view) {
         Intent intent = new Intent(this, SettingsActivity.class);
         startActivity(intent);
+    }
+
+    private String formatTime(int seconds) {
+        int minutesPart = seconds / 60;
+        int secondsPart = seconds % 60;
+        return String.format("%02d:%02d", minutesPart, secondsPart);
+    }
+
+    public void showCheckpointFrequencyMenu(View v) {
+        final int[] timeOptions = {5, 10, 30, 45, 60, 90, 120, 180, 240, 300};
+        final String[] timeOptionsString = new String[timeOptions.length];
+        for (int i = 0; i < timeOptions.length; ++i) {
+            timeOptionsString[i] = String.valueOf(timeOptions[i]);
+        }
+        // final TextView checkpointFrequencyDisplay = (TextView) findViewById(R.id.checkpointFrequencyValue);
+        new AlertDialog.Builder(this)
+                .setTitle(R.string.checkpoint_frequency_menu_title)
+                .setItems(timeOptionsString, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        SharedPreferences prefs =
+                                getSharedPreferences(getString(R.string.preference_file_key), Context.MODE_PRIVATE);
+                        SharedPreferences.Editor editor = prefs.edit();
+                        editor.putInt(getString(R.string.checkpoint_frequency_key), timeOptions[which]);
+                        editor.commit();
+                        checkpointFrequencyMillis = timeOptions[which] * 1000;
+                        checkpointFrequencyTextView.setText(formatTime(timeOptions[which]));
+                    }
+                }).show();
+
+    }
+
+    public void toggleAdaptiveCheckpointFrequency(View v) {
+        new AlertDialog.Builder(this).setTitle("111").show();
+    }
+
+    public void toggleAdaptiveLoudness(View v) {
+        new AlertDialog.Builder(this).setTitle("222").show();
+    }
+
+    public void showAlertTypeMenu(View v) {
+        new AlertDialog.Builder(this).setTitle("333").show();
+    }
+
+    public void showToneSelectionMenu(View v) {
+        new AlertDialog.Builder(this).setTitle("444").show();
+    }
+
+    public void showRecordNewToneMenu(View v) {
+        new AlertDialog.Builder(this).setTitle("555").show();
     }
 
     @Override
