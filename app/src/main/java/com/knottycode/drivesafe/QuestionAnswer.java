87d44fd371146e97ad3d91912be75d5b7b44a715
Knot <thammaknot@gmail.com>
2017-03-05 12:02:38 +0700

    done

--- app/src/main/java/com/knottycode/drivesafe/QuestionAnswer.java
+++ app/src/main/java/com/knottycode/drivesafe/QuestionAnswer.java
@@ -1,7 +1,9 @@
 package com.knottycode.drivesafe;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 /**
@@ -11,7 +13,10 @@ public class QuestionAnswer {
     private String question;
     private String answer;
     private QuestionType type;
-    private Set<String> answerKeywords;
+    private List<Set<String>> allAnswers;
+
+    private static final String ANSWER_ALTERNATIVES_DELIM = "\\|";
+    private static final String ANSWER_SYLLABLE_DELIM = ",";
 
     public enum QuestionType {
         FUNNY, RIDDLE, TRIVIA, OTHER;
@@ -21,12 +26,17 @@ public class QuestionAnswer {
         question = q;
         answer = a;
         this.type = type;
-        String[] tokens = keywords.split(",");
-        answerKeywords = new HashSet<String>(Arrays.asList(tokens));
+        String[] alts = keywords.split(ANSWER_ALTERNATIVES_DELIM);
+        allAnswers = new ArrayList<>();
+        for (String alt : alts) {
+            String[] tokens = alt.split(ANSWER_SYLLABLE_DELIM);
+            Set<String> answerKeywords = new HashSet<String>(Arrays.asList(tokens));
+            allAnswers.add(answerKeywords);
+        }
     }
 
     public String getQuestion() { return question; }
     public String getAnswer() { return answer; }
     public QuestionType getType() { return type; }
-    public Set<String> getAnswerKeywords() { return answerKeywords; }
+    public List<Set<String>> getAllAnswers() { return allAnswers; }
 }
