2615fb912dbd7c1ab74b0ec24ce06a6b2f994599
Knot <thammaknot@gmail.com>
2017-03-28 21:45:33 +0700

    rm unused

--- app/src/main/java/com/knottycode/drivesafe/TTSSupportActivity.java
+++ app/src/main/java/com/knottycode/drivesafe/TTSSupportActivity.java
@@ -70,13 +70,4 @@ public class TTSSupportActivity extends AppCompatActivity {
         // Close this Activity.
         finish();
     }
-/*
-    final String appPackageName = Constants.TTS_PACKAGE_NAME; // getPackageName() from Context or Activity object
-        try {
-            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + appPackageName)));
-        } catch (android.content.ActivityNotFoundException anfe) {
-            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=" + appPackageName)));
-        }
-    }
-    */
 }
